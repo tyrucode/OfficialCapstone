66cd2e1500d391e2f670c1e6f2824c81e094b8d8
ruizt <ruiztyler24@gmail.com>
2025-03-22 13:48:27 -0400

    testing signin with vercel

--- src/services/spotifyAuth.js
+++ src/services/spotifyAuth.js
@@ -2,9 +2,10 @@
 const CLIENT_ID = '619cbfa18fd846088bc95b31a67816b2';
 
 // Your redirect URI - use the one that's actually working
-const REDIRECT_URI = window.location.hostname === 'localhost'
-    ? 'http://localhost:5173/callback'
-    : 'https://official-capstone.vercel.app/callback'
+// const REDIRECT_URI = 'http://localhost:5173/callback'
+//ONE IS FOR VERCEL TESTING ONE IS FOR LOCALHOST TESTING, COMMENT OUT WHICHEVER ISNT IN USE
+const REDIRECT_URI = 'https://official-capstone.vercel.app/callback'
+
 
 // The permissions we're requesting from the user
 const SCOPES = [
