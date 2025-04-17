494262d8ed22c4acda72bdfffac39c11b45f72c6
ruizt <ruiztyler24@gmail.com>
2025-04-17 18:18:36 -0400

    test

--- src/lib/connectToDatabase.js
+++ src/lib/connectToDatabase.js
@@ -1,33 +1,23 @@
+import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+
+//load enviornmental variables from .env
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
 export async function connectToDatabase() {
-    if (cached.conn) {
-        console.log("Using cached database connection");
-        return { db: cached.conn, mongoClient: cached.conn.connection.client };
-    }
-
-    if (!cached.promise) {
-        console.log("Attempting to connect to MongoDB at:", MONGODB_URI);
-        const opts = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        };
-
-        cached.promise = mongoose.connect(MONGODB_URI, opts)
-            .then((mongoose) => {
-                console.log("MongoDB connection successful!");
-                return mongoose;
-            })
-            .catch(err => {
-                console.error("MongoDB connection error:", err);
-                throw err;
-            });
+    if (!uri) {
+        console.error('MONGODB_URI is not defined in environment variables');
+        return;
     }
 
     try {
-        cached.conn = await cached.promise;
-        console.log("Database connection established");
-        return { db: cached.conn, mongoClient: cached.conn.connection.client };
-    } catch (error) {
-        console.error("Failed to establish database connection:", error);
-        throw error;
+        const client = new MongoClient(uri, options);
+        await client.connect();
+
+    } catch (e) {
+        console.error('Connection failed:', e);
     }
 }
