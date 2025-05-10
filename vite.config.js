807c79231f6e73a1f99e0f92e3dddd698109e82a
ruizt <ruiztyler24@gmail.com>
2025-05-10 16:47:51 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      'react': 'https://cdn.skypack.dev/react@17',
-      'react-dom': 'https://cdn.skypack.dev/react-dom@17'
-    }
-  },
-  plugins: [reactRefresh()]
-});
+  plugins: [react()],
+})
