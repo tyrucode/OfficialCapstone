9bf408e6f4ae91fcefd1938cf709f1bb097a81d2
ruizt <ruiztyler24@gmail.com>
2025-05-20 19:42:07 -0400

    added a preview video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,6 +23,14 @@ function Home() {
                 </div>
             </div>
             <h1>A Spotify Premium subscription is required to play this game.</h1>
+            <h1>Due to API limitations, if you would like to try this app, I will need your Spotify email, which you can send me using one of the links below in the footer.</h1>
+            <h1>In the meantime, scroll down & enjoy a demo video below!</h1>
+            <iframe width="1200" height="600"
+                src="https://www.youtube.com/embed/2GEESgQlijA"
+                title="YouTube video player"
+                frameborder="1"
+                allowfullscreen>
+            </iframe>
         </div>
     )
 }
