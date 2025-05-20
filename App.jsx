2ba2a34175ac32f21102b2376b7cdda6c7191cdb
ruizt <ruiztyler24@gmail.com>
2025-05-19 21:26:20 -0400

    test

--- App.jsx
+++ App.jsx
@@ -1,16 +1,16 @@
 import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import { Analytics } from "@vercel/analytics/react"
 // pages
-import Home from "./pages/Home"
-import PlaylistDisplay from "./pages/PlaylistDisplay"
-import SpotifyCallback from "./pages/SpotifyCallback"
-import NotFound from "./pages/NotFound"
-import GameTimePage from "./pages/GameTimePage"
-import Leaderboard from "./pages/Leaderboard"
+import Home from "./src/pages/Home"
+import PlaylistDisplay from "./src/pages/PlaylistDisplay"
+import SpotifyCallback from "./src/pages/SpotifyCallback"
+import NotFound from "./src/pages/NotFound"
+import GameTimePage from "./src/pages/GameTimePage"
+import Leaderboard from "./src/pages/Leaderboard"
 // layouts
-import RootLayout from "./layouts/RootLayout"
+import RootLayout from "./src/layouts/RootLayout"
 // context
-import { UserProvider } from "./context/UserContext";
+import { UserProvider } from "./src/context/UserContext";
 
 // routes for all pages
 const router = createBrowserRouter(
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',     // Vercel expects this
+    rollupOptions: {
+      input: './index.html' // Tell Vite where to start
+    }
   }
 })
