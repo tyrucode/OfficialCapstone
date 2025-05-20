cd40a637a780b1de6482104b0945fb37a81befe1
ruizt <ruiztyler24@gmail.com>
2025-05-19 21:14:56 -0400

    test

--- main.jsx
+++ main.jsx
@@ -1,3 +1,4 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -6,17 +7,10 @@ import './App.css'
 
 import App from './App'
 
-// Make sure the DOM is loaded before trying to access 'root'
-const root = document.getElementById('root');
-
-if (root) {
-  createRoot(root).render(
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>,
-  );
-} else {
-  console.error("Root element not found - check your index.html");
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
