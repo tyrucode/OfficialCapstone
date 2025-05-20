32907dfa74a5d1372387a881583c29ad9d57ad62
ruizt <ruiztyler24@gmail.com>
2025-05-19 20:47:34 -0400

    testomg

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router';
 
 const NotFound = () => {
     // state for the not found page
--- src/pages/PlaylistDisplay.jsx
+++ src/pages/PlaylistDisplay.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import PlaylistGrid from "../components/PlaylistGrid"
 
 function Game() {
