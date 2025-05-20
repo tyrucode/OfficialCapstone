35840b11a4f1610e675b78314af21a72e645913b
ruizt <ruiztyler24@gmail.com>
2025-05-19 21:15:36 -0400

    fix

--- main.jsx
+++ main.jsx
@@ -1,7 +1,5 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 import './App.css'
 
@@ -9,8 +7,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
+
